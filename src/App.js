c5431b314189e2a3cc0f7ae16e20ccb85b438941
Naveen Mohanty <softwareengineer12@bidonhomes.com>
2022-09-09 00:26:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./main";
+import "./styles.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [show, setShow] = useState(false);
+  const [type, setType] = useState('success');
+  const [toastMsg, setToastMsg] = useState("")
+  const openTost = (msg = '', type = 'success', time = 3000) => {
+    setToastMsg(msg)
+    setShow(true)
+    setType(type)
+    setTimeout(() => {
+      closeTost()
+    }, time)
+  }
+  const closeTost = () => {
+    setToastMsg("")
+    setShow(false)
+    setType('success')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Main openTost={openTost} closeTost={closeTost} />
+      <div className="toast-container position-fixed bottom-0 end-0 p-3">
+        <div
+          id="liveToast"
+          className={"toast" + (show ? ' show' : '') + (type === 'success' ? ' text-bg-success' : type === 'error' ? ' text-bg-danger' : '')}
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className="d-flex">
+            <div className="toast-body">{toastMsg}</div>
+            <button
+              type="button"
+              className="btn-close me-2 m-auto"
+              data-bs-dismiss="toast"
+              aria-label="Close"
+              onClick={closeTost}
+            />
+          </div>
+        </div>
+      </div>
+    </>);
 }
-
-export default App;
